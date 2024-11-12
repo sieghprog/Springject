9c98f398ec14582232c93409f298f0cd10b44707
Arcsieghart <caiosiegh.prog@gmail.com>
2024-11-12 11:19:05 -0300

    Test config

---
+++ curso/src/main/java/com/arcsieghart/curso/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.arcsieghart.curso.repositories;
+
+import com.arcsieghart.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
